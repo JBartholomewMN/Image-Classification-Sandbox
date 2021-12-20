f316e5aff6f57ceb7c4bc371679ff9b58f407c48
John <jbartholomew6@gatech.edu>
2021-12-20 08:16:13 -0600

    pretrain backbone working with ViTs

--- pretrain.py
+++ pretrain.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 from config.config import CFG
 import tqdm
 from model.eval import _evaluate
+import torchmetrics
 
 
 def collate_fn(batch):
@@ -20,7 +21,9 @@ def collate_fn(batch):
 
 def train(model, optimizer, criterion, trainloader, testloader, epochs, device, cfg):
 
+    best_acc = 0
     model.train()
+    metric = torchmetrics.Accuracy()
     running_loss = 0.0
     for epoch in range(epochs):
         for i, (img_batch, label_batch) in enumerate(
@@ -41,8 +44,16 @@ def train(model, optimizer, criterion, trainloader, testloader, epochs, device, 
                 print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 100))
                 running_loss = 0.0
 
-                print(label_batch)
-                print(predictions.argmax(1))
+        for timgb, tlabs in tqdm.tqdm(testloader, desc="Testing"):
+            with torch.no_grad():
+                acc = metric(model(timgb.to(device)).detach().cpu(), tlabs)
+
+        acc = metric.compute()
+        print("accuracy: ", acc)
+
+        if acc > best_acc:
+            best_acc = acc
+            torch.save(model.state_dict(), "best.pt")
 
 
 if __name__ == "__main__":
@@ -70,13 +81,28 @@ if __name__ == "__main__":
         pin_memory=True,
     )
 
+    im_test_data = ImageNetClassifcation(
+        atransforms=CFG["A_transforms"],
+        root="dataset/data/imagenet_test",
+        transform=CFG["T_transforms"],
+        split="val",
+    )
+    testloader = torch.utils.data.DataLoader(
+        im_test_data,
+        batch_size=CFG["batch_size"],
+        shuffle=True,
+        num_workers=CFG["nworkers"],
+        collate_fn=collate_fn,
+        pin_memory=True,
+    )
+
     criterion = torch.nn.CrossEntropyLoss()
     train(
         model,
         optimizer,
         criterion,
         trainloader,
-        None,
+        testloader,
         CFG["epochs"],
         CFG["device"],
         CFG,
